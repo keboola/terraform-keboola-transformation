6050c16a4c8ff33c092832cff0f66f31854aec4b
Martin Vaško <Matovidlo2@gmail.com>
2025-05-12 11:05:53 +0200

    fix: Add configuration ID output

--- outputs.tf
+++ outputs.tf
@@ -16,4 +16,9 @@ output "component_id" {
 output "change_description" {
   description = "The change description associated with the latest version of the configuration."
   value       = keboola_component_configuration.this.change_description
+}
+
+output "configuration_id" {
+  description = "The configuration ID of the created Keboola component configuration."
+  value       = keboola_component_configuration.this.configuration_id
 } 
